3c88a055844078f10d39cb02e355e4877432a10a
kirill <nicknamekirill@gmail.com>
2023-03-09 15:52:36 +0300

    complete

--- homework29/Program.cs
+++ homework29/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int [8];
+for (int i =0; i < 8; i++)
+{
+    array[i] = new Random().Next(0, 9);
+}
+Console.WriteLine($"[{String.Join(", " ,array)}]");
