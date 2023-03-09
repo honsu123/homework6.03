b7aa06c095f3c1b350408e75be693889aa83a9a2
kirill <nicknamekirill@gmail.com>
2023-03-09 16:18:16 +0300

    finish

--- homework25/Program.cs
+++ homework25/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите два числа");
+double N1 = Convert.ToDouble(Console.ReadLine());
+double N2 = Convert.ToDouble(Console.ReadLine());
+double sum = Math.Pow(N1, N2);
+Console.WriteLine(sum);
